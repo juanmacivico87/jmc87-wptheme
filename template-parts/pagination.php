9e2deb81fe8adda68becd7043b7180e1bd5a4739
juanmacivico87 <juanmacivico87@gmail.com>
2019-07-10 14:54:52 +0200

    [PAGINATION] Add pagination WordPress function

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -7,5 +7,14 @@
  * @package jmc87-wptheme
  */
 
- //https://codex.wordpress.org/Pagination
+ //https://developer.wordpress.org/themes/functionality/pagination/
+ //https://developer.wordpress.org/reference/functions/the_posts_pagination/
  //https://codex.wordpress.org/Function_Reference/paginate_links
+
+$args = array(
+    'mid_size'  => 2,
+    'prev_text' => __( '<', THEME_TEXTDOMAIN ),
+    'next_text' => __( '>', THEME_TEXTDOMAIN ),
+);
+
+the_posts_pagination( $args );
