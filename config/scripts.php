6cb08275011573eeab33d5cebd1b7196f0eeed90
juanmacivico87 <juanmacivico87@gmail.com>
2019-08-03 13:46:51 +0200

    Add credits.js file

--- config/scripts.php
+++ config/scripts.php
@@ -17,6 +17,7 @@ class JMC87_LoadScripts
 
     function jmc87_load_scripts()
     {
+        wp_enqueue_script( 'credits-js', get_template_directory_uri() . '/inc/js/credits.js', array(), false, true );
         wp_enqueue_script( 'jmc87-js', get_template_directory_uri() . '/inc/js/js-file.js', array(), false, true );
     }
 }
