8978f8123660eb0ec7ddf25030f5e86da344e51e
juanmacivico87 <juanmacivico87@gmail.com>
2019-08-03 14:08:25 +0200

    Update theme credits

--- inc/js/credits.js
+++ inc/js/credits.js
@@ -1,1 +1,5 @@
-console.log( 'Developed by JuanMaCivico87 - wwww.juanmacivico87.com - hola@juanmacivico87.com' );
+console.group( 'Credits' )
+console.log( 'Theme developed by JuanMaCivico87 - www.juanmacivico87.com - hola@juanmacivico87.com' );
+console.log( 'Visit my GitHub profile: https://github.com/juanmacivico87' );
+console.log( 'Follow me on Twitter: https://twitter.com/juanmacivico87' );
+console.log( 'Visit my WordPress profile: https://profiles.wordpress.org/webappdesign/' );
