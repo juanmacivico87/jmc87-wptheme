71289fd4a787723f02e792ccd3815dc104c71051
juanmacivico87 <juanmacivico87@gmail.com>
2019-08-06 20:30:48 +0200

    Render post category taxonomy

--- src/posts/views/taxonomy-category.php
+++ src/posts/views/taxonomy-category.php
@@ -9,7 +9,23 @@
 
 if ( have_posts() ) : 
     while ( have_posts() ) : the_post();
-    endwhile;
+        $categories = get_the_terms( $post->ID, 'category' );
+        $post_tags  = get_the_terms( $post->ID, 'post_tag' ); ?>
+
+        <a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><img src="<?php echo get_the_post_thumbnail_url() ?>" alt=""></a>
+        <h3><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h3>
+        <p><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_excerpt() ?></a></p>
+        <p><?php echo get_the_author() ?></p>
+
+        <p><?php _e( 'Tags:', THEME_TEXTDOMAIN ) ?></p>
+        <ul>
+            <?php if ( $post_tags ) :
+                foreach( $post_tags as $tag ) : ?>
+                    <li><a href="<?php echo get_term_link( $tag->term_id, $tag->taxonomy ) ?>" title="<?php echo $tag->name ?>"><?php echo $tag->name ?></a></li>
+                <?php endforeach;
+            endif; ?>
+        </ul>
+    <?php endwhile;
     get_template_part( 'template-parts/pagination' );
 else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts found', THEME_TEXTDOMAIN ); ?></p>
