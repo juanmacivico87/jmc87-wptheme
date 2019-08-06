a94147549935d58a5746b126a00ab0d8e3c817b2
juanmacivico87 <juanmacivico87@gmail.com>
2019-08-06 20:57:00 +0200

    Render post search page

--- search.php
+++ search.php
@@ -11,14 +11,11 @@ get_header();
 
 $post_type = get_post_type();
 switch( $post_type ) {
-	case 'post':
-		get_template_part( 'src/posts/views/search', $post_type );
-		break;
 	case 'sample':
 		get_template_part( 'src/sample/views/search', $post_type );
 		break;
 	default:
-		_e( 'Post type search page not found', THEME_TEXTDOMAIN );
+		get_template_part( 'template-parts/default', 'search' );
 		break;
 }
 
