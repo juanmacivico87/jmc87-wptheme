215d7c17c1b54fbfc73dc64e8a6b210186bd5457
juanmacivico87 <juanmacivico87@gmail.com>
2020-09-19 11:00:40 +0200

    Add template sample of single page with blocks

---
+++ template-samples/single-post.php
@@ -0,0 +1,245 @@
+<!-- Heading -->
+<h1>Título H1</h1>
+<h2>Título H2</h2>
+<h3>Título H3</h3>
+<h4>Título H4</h4>
+<h5>Título H5</h5>
+<h6>Título H6</h6>
+<!-- Separator -->
+<hr class="wp-block-separator">
+<!-- Buttons -->
+<div class="wp-block-buttons">
+    <div class="wp-block-button">
+        <a class="wp-block-button__link">This is a Button</a>
+    </div>
+    <div class="wp-block-button">
+        <a class="wp-block-button__link">This is an other button</a>
+    </div>
+</div>
+<!-- Spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- Media & Text -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile">
+    <figure class="wp-block-media-text__media">
+        <img src="http://wp.test.com/wp-content/uploads/2020/05/delegacion-granada-wcmad-1024x768.jpg" alt="" class="wp-image-74" srcset="http://wp.test.com/wp-content/uploads/2020/05/delegacion-granada-wcmad-1024x768.jpg 1024w, http://wp.test.com/wp-content/uploads/2020/05/delegacion-granada-wcmad-300x225.jpg 300w, http://wp.test.com/wp-content/uploads/2020/05/delegacion-granada-wcmad-768x576.jpg 768w, http://wp.test.com/wp-content/uploads/2020/05/delegacion-granada-wcmad-1200x900.jpg 1200w, http://wp.test.com/wp-content/uploads/2020/05/delegacion-granada-wcmad.jpg 1280w" sizes="(max-width: 1024px) 100vw, 1024px">
+    </figure>
+    <div class="wp-block-media-text__content">
+        <h2>Granaínos en WordCamp Madrid</h2>
+        <p>Lorem ipsum dolor sit amet, con una consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum.</p>
+        <div class="wp-block-buttons">
+            <div class="wp-block-button"><a class="wp-block-button__link">Enlace</a></div>
+        </div>
+    </div>
+</div>
+<!-- Paragraph -->
+<p>Esto es un párrafo alineado a la izquierda lorem ipsum dolor sit amet, con una consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum. Suspendisse a ultricies velit. Donec gravida ut leo id iaculis. Suspendisse quis augue gravida, blandit lacus ac, sollicitudin risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque metus magna, lacinia ac posuere a, pellentesque sit amet orci.</p>
+<p class="has-text-align-center">Esto es un párrafo alineado al centro lorem ipsum dolor sit amet, con una consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum. Suspendisse a ultricies velit. Donec gravida ut leo id iaculis. Suspendisse quis augue gravida, blandit lacus ac, sollicitudin risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque metus magna, lacinia ac posuere a, pellentesque sit amet orci.</p>
+<p class="has-text-align-right">Esto es un párrafo alineado a la derecha lorem ipsum dolor sit amet, con una consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum. Suspendisse a ultricies velit. Donec gravida ut leo id iaculis. Suspendisse quis augue gravida, blandit lacus ac, sollicitudin risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque metus magna, lacinia ac posuere a, pellentesque sit amet orci.</p>
+<!-- Image -->
+<div class="wp-block-image">
+    <figure class="aligncenter">
+        <img src="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg" alt="Juan Manuel Civico Cabrera" class="wp-image-6" srcset="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg 800w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-300x300.jpg 300w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-150x150.jpg 150w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-768x768.jpg 768w" sizes="(max-width: 800px) 100vw, 800px">
+        <figcaption>Esto es una imagen, alineada al centro, con leyenda</figcaption>
+    </figure>
+</div>
+<div class="wp-block-image">
+    <figure class="alignleft">
+        <img src="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg" alt="Juan Manuel Civico Cabrera" class="wp-image-6" srcset="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg 800w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-300x300.jpg 300w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-150x150.jpg 150w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-768x768.jpg 768w" sizes="(max-width: 800px) 100vw, 800px">
+        <figcaption>Esta es una imagen, alienada a la izquierda, con leyenda</figcaption>
+    </figure>
+</div>
+<div class="wp-block-image">
+    <figure class="alignright">
+        <img src="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg" alt="Juan Manuel Civico Cabrera" class="wp-image-6" srcset="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg 800w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-300x300.jpg 300w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-150x150.jpg 150w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-768x768.jpg 768w" sizes="(max-width: 800px) 100vw, 800px">
+        <figcaption>Esta es una imagen, alienada a la derecha, con leyenda</figcaption>
+    </figure>
+</div>
+<!-- List -->
+<ul>
+    <li>Este es el primer elemento de una lista desordenada</li>
+    <li>Y este es el segundo elemento</li>
+    <li>Este es el tercer elemento</li>
+    <li>Y así hasta el infinito y más allá</li>
+</ul>
+<ol>
+    <li>Este es el primer elemento de una lista ordenada</li>
+    <li>Y este es el segundo elemento</li>
+    <li>Este es el tercero</li>
+    <li>Y a ver si adivinas cual es este</li>
+</ol>
+<!-- Blockquote -->
+<blockquote class="wp-block-quote">
+    <p>Esto es una cita lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum.</p>
+    <cite>Juan Manuel Civico Cabrera</cite>
+</blockquote>
+<!-- Gallery -->
+<figure class="wp-block-gallery columns-3 is-cropped">
+    <ul class="blocks-gallery-grid">
+        <li class="blocks-gallery-item">
+            <figure>
+                <img src="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg" alt="Juan Manuel Civico Cabrera" data-id="6" data-link="http://wp.test.com/meetup/meetup-de-abril/juanmacivico87/" class="wp-image-6" srcset="http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87.jpg 800w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-300x300.jpg 300w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-150x150.jpg 150w, http://wp.test.com/wp-content/uploads/2020/04/juanmacivico87-768x768.jpg 768w" sizes="(max-width: 800px) 100vw, 800px">
+            </figure>
+        </li>
+        <li class="blocks-gallery-item">
+            <figure>
+                <img src="http://wp.test.com/wp-content/uploads/2020/04/evagarcia.jpg" alt="" data-id="7" data-link="http://wp.test.com/meetup/meetup-de-abril/evagarcia/" class="wp-image-7" srcset="http://wp.test.com/wp-content/uploads/2020/04/evagarcia.jpg 512w, http://wp.test.com/wp-content/uploads/2020/04/evagarcia-300x300.jpg 300w, http://wp.test.com/wp-content/uploads/2020/04/evagarcia-150x150.jpg 150w" sizes="(max-width: 512px) 100vw, 512px">
+            </figure>
+        </li>
+        <li class="blocks-gallery-item">
+            <figure>
+                <img src="http://wp.test.com/wp-content/uploads/2020/04/carlosmdh.jpg" alt="" data-id="8" data-link="http://wp.test.com/meetup/meetup-de-abril/carlosmdh/" class="wp-image-8" srcset="http://wp.test.com/wp-content/uploads/2020/04/carlosmdh.jpg 512w, http://wp.test.com/wp-content/uploads/2020/04/carlosmdh-300x300.jpg 300w, http://wp.test.com/wp-content/uploads/2020/04/carlosmdh-150x150.jpg 150w" sizes="(max-width: 512px) 100vw, 512px">
+            </figure>
+        </li>
+        <li class="blocks-gallery-item">
+            <figure>
+                <img src="http://wp.test.com/wp-content/uploads/2020/05/juanmacivico87.png" alt="" data-id="70" data-link="http://wp.test.com/2020/05/14/titulo-h1/juanmacivico87-2/" class="wp-image-70" srcset="http://wp.test.com/wp-content/uploads/2020/05/juanmacivico87.png 800w, http://wp.test.com/wp-content/uploads/2020/05/juanmacivico87-300x300.png 300w, http://wp.test.com/wp-content/uploads/2020/05/juanmacivico87-150x150.png 150w, http://wp.test.com/wp-content/uploads/2020/05/juanmacivico87-768x768.png 768w" sizes="(max-width: 800px) 100vw, 800px">
+            </figure>
+        </li>
+        <li class="blocks-gallery-item">
+            <figure>
+                <img src="http://wp.test.com/wp-content/uploads/2020/05/campus-wcmad-1024x576.jpg" alt="" data-id="72" data-link="http://wp.test.com/2020/05/14/titulo-h1/campus-wcmad/" class="wp-image-72" srcset="http://wp.test.com/wp-content/uploads/2020/05/campus-wcmad-1024x576.jpg 1024w, http://wp.test.com/wp-content/uploads/2020/05/campus-wcmad-300x169.jpg 300w, http://wp.test.com/wp-content/uploads/2020/05/campus-wcmad-768x432.jpg 768w, http://wp.test.com/wp-content/uploads/2020/05/campus-wcmad-1200x675.jpg 1200w, http://wp.test.com/wp-content/uploads/2020/05/campus-wcmad.jpg 1280w" sizes="(max-width: 1024px) 100vw, 1024px">
+            </figure>
+        </li>
+    </ul>
+    <figcaption class="blocks-gallery-caption">Esto es una galería de fotos, con tres fotos arriba y dos abajo</figcaption>
+</figure>
+<!-- File -->
+<div class="wp-block-file">
+    <a href="http://wp.test.com/wp-content/uploads/2020/05/Ingreso-Subvención.pdf">Esto es un archivo para descargar</a>
+    <a href="http://wp.test.com/wp-content/uploads/2020/05/Ingreso-Subvención.pdf" class="wp-block-file__button" download="">Descargar</a>
+</div>
+<!-- Audio -->
+<figure class="wp-block-audio">
+    <audio controls="" src="http://wp.test.com/wp-content/uploads/2020/05/Fábula.mp3"></audio>
+    <figcaption>Esta es la leyenda de un archivo de audio</figcaption>
+</figure>
+<!-- Video -->
+<figure class="wp-block-video">
+    <video controls="" src="http://wp.test.com/wp-content/uploads/2020/05/Nerea-dice-murciegalo.mp4" data-origwidth="0" data-origheight="0" style="width: 580px;"></video>
+    <figcaption>Esto es la leyenda de un archivo de vídeo</figcaption>
+</figure>
+<!-- Code -->
+<pre class="wp-block-code">
+    <code>//Esto es un fragmento de código PHP
+    public function wpmad_mo_get_post_type_templates( $template )
+    {
+        if ( get_post_type() === $this-&gt;post_type )
+        { 
+            switch( true ) 
+            {
+                case is_archive() &amp;&amp; !is_tax():
+                    $template = WPMAD_MO_PLUGIN_DIR . 'src/customPostsTypes/meetupsPostType/views/archive-meetup.php';
+                    break;
+                case is_single():
+                    $template = WPMAD_MO_PLUGIN_DIR . 'src/customPostsTypes/meetupsPostType/views/single-meetup.php';
+                    break;
+            }
+        }
+
+        return $template;
+    }</code>
+</pre>
+<!-- Pullquote -->
+<figure class="wp-block-pullquote">
+    <blockquote>
+        <p>Esto es una cita con otro formato, alineada al centro lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum.</p>
+        <cite>Juan Manuel Civico Cabrera</cite>
+    </blockquote>
+</figure>
+<figure class="wp-block-pullquote alignwide">
+    <blockquote>
+        <p>Esto es una cita con formato de ancho amplio lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum.</p>
+        <cite>Juan Manuel Civico Cabrera</cite>
+    </blockquote>
+</figure>
+<figure class="wp-block-pullquote alignfull">
+    <blockquote>
+        <p>Esto es una cita con formato de ancho completo lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus mauris eget sem venenatis tincidunt. Cras condimentum lorem nec enim ultricies, non fringilla ligula fermentum.</p>
+        <cite>Juan Manuel Civico Cabrera</cite>
+    </blockquote>
+</figure>
+<figure class="wp-block-pullquote alignleft">
+    <blockquote>
+        <p>Esto es una cita con otro formato, alineada a la izquierda lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <cite>Juan Manuel Civico Cabrera</cite>
+    </blockquote>
+</figure>
+<figure class="wp-block-pullquote alignright">
+    <blockquote>
+        <p>Esto es una cita con otro formato, alineada a la derecha lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <cite>Juan Manuel Civico Cabrera</cite>
+    </blockquote>
+</figure>
+<!-- Table -->
+<figure class="wp-block-table">
+    <table>
+        <thead>
+            <tr>
+                <th>Colum 1</th>
+                <th>Colum 2</th>
+                <th>Colum 3</th>
+                <th>Colum 4</th>
+                <th>Colum 5</th>
+                <th>Colum 6</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1 – 1</td>
+                <td>1 – 2</td>
+                <td>1 – 3</td>
+                <td>1 – 4</td>
+                <td>1 – 5</td>
+                <td>1 – 6</td>
+            </tr>
+            <tr>
+                <td>2 – 1</td>
+                <td>2 – 2</td>
+                <td>2 – 3</td>
+                <td>2 – 4</td>
+                <td>2 – 5</td>
+                <td>2 – 6</td>
+            </tr>
+            <tr>
+                <td>3 – 1</td>
+                <td>3 – 2</td>
+                <td>3 – 3</td>
+                <td>3 – 4</td>
+                <td>3 – 5</td>
+                <td>3 – 6</td>
+            </tr>
+        </tbody>
+    </table>
+    <figcaption>Esto es una tabla</figcaption>
+</figure>
+<!-- Embed Twitter -->
+<figure class="wp-block-embed-twitter aligncenter wp-block-embed is-type-rich is-provider-twitter">
+    <div class="wp-block-embed__wrapper">
+        <twitter-widget class="twitter-tweet twitter-tweet-rendered" id="twitter-widget-0" style="position: static; visibility: visible; display: block; transform: rotate(0deg); max-width: 100%; width: 550px; min-width: 220px; margin-top: 10px; margin-bottom: 10px;" data-tweet-id="1260880222246899712"></twitter-widget>
+        <script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+    <figcaption>Esto es un enlace embebido desde Twitter</figcaption>
+</figure>
+<!-- Embed YouTube -->
+<figure class="wp-block-embed-youtube aligncenter wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+    <div class="wp-block-embed__wrapper">
+        <iframe title="Super Bikes Street Riding Go Pro Motorcycle" width="580" height="326" src="https://www.youtube.com/embed/qaBH-GGP8Pg?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-origwidth="580" data-origheight="326" style="width: 580px; height: 326px;"></iframe>
+    </div>
+    <figcaption>Esto es un vídeo de YouTube</figcaption>
+</figure>
+<!-- Embed WordPress -->
+<figure class="wp-block-embed-wordpress wp-block-embed is-type-wp-embed is-provider-juanma-civico">
+    <div class="wp-block-embed__wrapper">
+        <blockquote class="wp-embedded-content" data-secret="1QT0gD8wMl" style="display: none;">
+            <a href="https://www.juanmacivico87.com/metodologia-trabajo-git-flow/">Metodología de trabajo Git Flow. Divide tu proyecto en ramas y vencerás.</a>
+        </blockquote>
+        <iframe title="«Metodología de trabajo Git Flow. Divide tu proyecto en ramas y vencerás.» — JuanMa Civico" class="wp-embedded-content" sandbox="allow-scripts" security="restricted" src="https://www.juanmacivico87.com/metodologia-trabajo-git-flow/embed/#?secret=1QT0gD8wMl" data-secret="1QT0gD8wMl" width="580" height="387" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" data-origwidth="580" data-origheight="1000" style="width: 580px; height: 1000px;"></iframe>
+    </div>
+    <figcaption>Esto es un enlace embebido desde un blog hecho con WordPress</figcaption>
+</figure>
+<!-- Group -->
+<div class="wp-block-group">
+    <div class="wp-block-group__inner-container">
+        <!-- Blocks here -->
+    </div>
+</div>
